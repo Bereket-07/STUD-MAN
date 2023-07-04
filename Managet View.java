bf02f0dfded5aaa78e172f882f86e3a3c42bc967
unknown <teddyget1870@gmail.com>
2023-07-04 09:50:36 +0300

    df

--- Managet View.java
+++ Managet View.java
@@ -9,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
